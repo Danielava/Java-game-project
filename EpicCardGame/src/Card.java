3fd19d0107c7833f745e76ac0f0ce6c57ce658c4
Daniel <Mariostarr@n138-p201.kthopen.kth.se>
2016-05-04 16:28:04 +0200

    card class created

---
+++ EpicCardGame/src/Card.java
@@ -0,0 +1,16 @@
+/**
+ * The card class. It is responsible for every card that the
+ * player can put on the board.
+ * The card class contains:
+ * 1. One or more spells to use against enemies.
+ * 2. Health points, which if reduced to or below 0 results in the card
+ * being destroyed.
+ * 3. A picture of the card.
+ * 4. A name
+ *
+ * This class is supposed to create a card and then generate it to a visible thing
+ * which will be drawn on the canvas.
+ */
+public class Card {
+    
+}
