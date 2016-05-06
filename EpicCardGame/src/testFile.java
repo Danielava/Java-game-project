9c143035baa5c71f30f1a3d56b90e64a537e460e
TheForgot3n1 <gerdano_jetmotor@hotmail.com>
2016-05-06 13:57:25 +0200

    Experimentation with file handling Purely for experimentation at the moment (shared with my partner now).

---
+++ EpicCardGame/src/testFile.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.io.*;
+
+public class testFile {
+	public testFile() {
+		BufferedReader in = null;
+		try{
+			in = new BufferedReader(new FileReader("CuteGirl"));
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Did not work1");
+		}
+		if(in == null) return;
+		String line;
+		try {
+			while((line = in.readLine()) != null)
+				{
+					System.out.println(line);
+				}
+			in.close();
+		}
+		catch(IOException e) {
+			System.out.println("Did not work2");
+		}
+	}
+}
