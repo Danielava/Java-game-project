668866cf80a9478a716398c7b519b71b1d436751
Daniel <Mariostarr@Daniels-MacBook-Pro.local>
2016-05-08 00:55:53 +0200

    add methods draw & add

--- EpicCardGame/src/Deck.java
+++ EpicCardGame/src/Deck.java
@@ -1,10 +1,42 @@
 import java.util.*;
 
+/**
+ * The deck class is a object that's
+ * meant to hold a lot of cards.
+ * This class will communicate with the Hand class.
+ * Drawing cards from deck will put them in you Hand.
+ *
+ * The way this class will work is that the cards will all
+ * be inside a deck in a set order but when you draw one card,
+ * it will randomly pick one from the deck, making it look like
+ * the deck is shuffled
+ */
+
 public class Deck {
 	private ArrayList<Card> cards;
 	private final int deckSize = 20;
 	
 	public Deck() {
-		
+
 	}
+
+	/**
+	 * draws a card randomly from the deck
+	 * and puts it in your hand. It's basicly like a
+	 * remove function.
+	 */
+	public void draw() {
+
+	}
+
+	/**
+	 * add cards to the deck
+	 * @param card you wanna add
+     */
+	public void add(Card card) {
+
+	}
+
+
+
 }
