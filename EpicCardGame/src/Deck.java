ac028cb942979c874146d97fb5c389faf5be40a7
Daniel <Mariostarr@Daniels-MBP.lan>
2016-05-07 13:11:59 +0200

    add images for cards

---
+++ EpicCardGame/src/Deck.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.*;
+
+public class Deck {
+	private ArrayList<Card> cards;
+	private final int deckSize = 20;
+	
+	public Deck() {
+		
+	}
+}
---
+++ EpicCardGame/src/Type.java
@@ -0,0 +1,5 @@
+package main;
+
+public enum Type {	
+    DARK, MACHINE, JUSTICE, CUTE;
+}
