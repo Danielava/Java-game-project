398c524c51603d789ee04c83b21db5523e3aface
Daniel <Mariostarr@Daniels-MacBook-Pro.local>
2016-05-08 09:14:25 +0200

    comments added

--- EpicCardGame/src/Hand.java
+++ EpicCardGame/src/Hand.java
@@ -1,14 +1,22 @@
 import java.util.ArrayList;
 
+/**
+ * All cards in the hand should be visible on the lower center of the
+ * screen in a row.
+ */
 public class Hand {
 	private final int MAX = 5; //Amount of cards to be able to hold
-	private ArrayList<Card> cards;
+	private ArrayList<Card> hand;
 	
 	public Hand() {
-		
+		hand = new ArrayList<Card>();
 	}
-	
-	public void draw() {
-		
+
+	/**
+	 * Makes so all cards in hand is sorted in a row at the bottom of the screen.
+	 * The trick is to but them in a Hbox.
+	 */
+	public void sort() {
+
 	}
 }
