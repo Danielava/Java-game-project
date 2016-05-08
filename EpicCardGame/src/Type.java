f55d341972c3e0021b0a6c40bdd936cad889aed5
Daniel <Mariostarr@Daniels-MacBook-Pro.local>
2016-05-08 13:42:24 +0200

    define some types

--- EpicCardGame/src/Type.java
+++ EpicCardGame/src/Type.java
@@ -1,3 +1,12 @@
+/**
+ * These are basically our card types in this game.
+ * In pokemon these would for example be type fire,
+ * water, grass, electric etc. We felt like those
+ * types are kind of generic so we wanted to come
+ * up with our own unique types
+ */
+
+//type BAD is more like badass/cool. not bad as in evil..
 public enum Type {
-    DARK, MACHINE, JUSTICE, CUTE;
+    HAPPY, BAD, BRAVE, CUTE, TALENTED
 }
