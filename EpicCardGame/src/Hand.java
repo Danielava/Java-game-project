961fb20b084ad8fea70b7661cfdd449314adadc9
Daniel <Mariostarr@Daniels-MBP.lan>
2016-05-09 14:58:34 +0200

    drawVisuals method implement

--- EpicCardGame/src/Hand.java
+++ EpicCardGame/src/Hand.java
@@ -1,3 +1,5 @@
+import javafx.scene.layout.HBox;
+
 import java.util.ArrayList;
 
 /**
@@ -7,17 +9,45 @@ import java.util.ArrayList;
 public class Hand {
 	private final int MAX = 5; //Amount of cards to be able to hold
 	private ArrayList<Card> hand;
-	
+
+	private HBox mainBox;
+
 	public Hand() {
 		hand = new ArrayList<Card>();
+		mainBox = new HBox();
 	}
 
 	/**
+	 * Add a card to hand
+	 */
+	public void add(Card card) {
+		if(hand.size() >= MAX) {
+			return;
+		}
+		hand.add(card);
+		drawVisuals();
+	}
+
+	/*
+	You don't need the root. All you need to do is switch the position
+	of the cards from the deck to the HBox.
+	 */
+
+	/**
 	 * Makes so all cards in hand is sorted in a row at the bottom of the screen.
-	 * The trick is to but them in a Hbox.
+	 * The trick is to put them in a Hbox.
+	 */
+	/*
+
 	 */
 	//this method should be called in the gameLoop.
-	public void draw() {
-
+	public void drawVisuals() {
+		for(Card c : hand) {
+			mainBox.getChildren().add(c.getVBoxCard());
+			c.show();
+		}
 	}
+
+
+
 }
