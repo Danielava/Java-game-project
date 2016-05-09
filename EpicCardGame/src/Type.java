b4306e444f7ac307f80e4975e968ed9339a22b2d
Daniel <Mariostarr@Daniels-MBP.lan>
2016-05-09 22:26:07 +0200

    add comment

--- EpicCardGame/src/Type.java
+++ EpicCardGame/src/Type.java
@@ -4,6 +4,11 @@
  * water, grass, electric etc. We felt like those
  * types are kind of generic so we wanted to come
  * up with our own unique types
+ *
+ * It would be natural to think that a card Type would affect
+ * gameplay but we still aren't sure about that.
+ * What the types will do however is that it will affect the color
+ * of the card.
  */
 
 //type BAD is more like badass/cool. not bad as in evil..
