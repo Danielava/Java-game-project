95f6a2ada2e8e8ae110157d49000dbaa26473ece
Daniel <Mariostarr@Daniels-MBP.lan>
2016-05-09 22:20:43 +0200

    card is flipped before drawn

--- EpicCardGame/src/Hand.java
+++ EpicCardGame/src/Hand.java
@@ -1,4 +1,5 @@
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
 
@@ -7,25 +8,36 @@ import java.util.ArrayList;
  * screen in a row.
  */
 public class Hand {
-	private final int MAX = 5; //Amount of cards to be able to hold
+	public final int MAX = 5; //Amount of cards to be able to hold
 	private ArrayList<Card> hand;
+	private int xPos, yPos;
 
-	private HBox mainBox;
+	private HBox mainBox; //the box where hand is held
 
 	public Hand() {
 		hand = new ArrayList<Card>();
 		mainBox = new HBox();
+		mainBox.setTranslateX(0);
+		mainBox.setTranslateY(0);
 	}
 
 	/**
 	 * Add a card to hand
 	 */
 	public void add(Card card) {
+
 		if(hand.size() >= MAX) {
 			return;
 		}
 		hand.add(card);
 		drawVisuals();
+	}
+
+	/**
+	 * returns the nr of cards currently held in hand.
+     */
+	public int getCardsInHand() {
+		return hand.size();
 	}
 
 	/*
@@ -38,16 +50,21 @@ public class Hand {
 	 * The trick is to put them in a Hbox.
 	 */
 	/*
-
-	 */
+		GLÖM EJ ATT BYTA KORTETS X OCH Y POSITION TILL BOXEN
+	*/
 	//this method should be called in the gameLoop.
 	public void drawVisuals() {
 		for(Card c : hand) {
-			mainBox.getChildren().add(c.getVBoxCard());
+			c.flipCard();
+			c.generateCard(c.getRoot());
+			VBox card = c.getVBoxCard(); //ger dendär bakgrunden
+			//experiment with these variables to put in hand.
+			//write a good algorithm.
+			card.setTranslateY(100);
+			card.setTranslateX(100);
+
+			mainBox.getChildren().add(card);
 			c.show();
 		}
 	}
-
-
-
 }
