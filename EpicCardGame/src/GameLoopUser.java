b09008b2c4441a5aa265626c73fabf2f252b8b49
Daniel <Mariostarr@Daniels-MBP.lan>
2016-05-09 14:59:20 +0200

    more code added

--- EpicCardGame/src/GameLoopUser.java
+++ EpicCardGame/src/GameLoopUser.java
@@ -1,4 +1,5 @@
 import javafx.animation.AnimationTimer;
+import javafx.scene.Scene;
 
 /**
  * This is the main GameLoop
@@ -8,25 +9,38 @@ import javafx.animation.AnimationTimer;
  * Also the card should then be flipped and visible, and you can't have more than 5 cards
  * in your hand.
  *
+ * THIS GAME LOOP CLASS IS MEANT FOR THE USER (YOU) ONLY.
+ * THIS IS NOT THE AI. SEE THIS AS THE USER INTERFACE
  */
-public class GameLoopUser {
+public class GameLoopUser{
+
+    private Deck myDeck;
+    private Scene scene;
 
     //constructor
-    public GameLoop() {
-
+    public GameLoopUser(Deck deck, Scene scene) {
+        myDeck = deck;
+        this.scene = scene;
     }
 
 
+
+
     public void startGame() {
+
+        scene.getOnKeyPressed();
 
         new AnimationTimer() {
 
             @Override
             public void handle(long time) {
 
+
+
+
             }
+
+
         }.start();
     }
-
-
 }
