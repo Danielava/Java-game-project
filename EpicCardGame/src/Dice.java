f8c07b55addd37c5d11d990a113461a281f4ba2b
Rafael Dolfe <rafaeld@kth.se>
2016-05-11 19:46:26 +0200

    Add Dice class' fields and constructor

--- EpicCardGame/src/Dice.java
+++ EpicCardGame/src/Dice.java
@@ -5,6 +5,22 @@
  */
 public class Dice {
 
-
-
+	private Random r;
+	private final int diceRolls = 30;
+	
+	public Dice(){
+		r = new Random();
+	}
+	
+	/*
+	 * generates an array pattern of how
+	 * the random dice roll will look like.
+	 */
+	public int[] generateRandomPattern() {
+		int[] array = new int[diceRolls];
+		for(int i=0; i<diceRolls; i++) {
+			array[i] = r.nextInt(6)+1;
+		}
+		return array;
+	}
 }
