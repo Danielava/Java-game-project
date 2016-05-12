05a55a12bddfc83a54026b83c8b78a6efd2100fd
Daniel <dpar@kth.se>
2016-05-12 18:37:03 +0200

    dice class

--- EpicCardGame/src/Dice.java
+++ EpicCardGame/src/Dice.java
@@ -1,3 +1,14 @@
+import java.util.Random;
+import java.util.ArrayList;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+/**
+ * This class will create a dice on the screen.
+ * Click on the dice to roll it and then you will be able to perform
+ * your move.
+ */
 /**
  * This class will create a dice on the screen.
  * Click on the dice to roll it and then you will be able to perform
@@ -5,6 +16,32 @@
  */
 public class Dice {
 
-
-
+	private Random r;
+	private final int diceRolls = 30;
+	
+	public Dice(){
+		r = new Random();
+	}
+	
+	/*
+	 * generates an array pattern of how
+	 * the random dice roll will look like.
+	 */
+	public int[] generateRandomPattern() {
+		int[] array = new int[diceRolls];
+		for(int i=0; i<diceRolls; i++) {
+			array[i] = r.nextInt(6)+1;
+		}
+		return array;
+	}
+	public void displayDiceRolling() {
+		AnimatedImage dice = new AnimatedImage();
+		Image[] imageArray = new Image[diceRolls];
+		for (int i = 0; i < diceRolls; i++) {
+		    imageArray[i] = new Image( "dice" + i + ".png" );
+		    
+		}
+		dice.frames = imageArray;
+		dice.duration = 0.500;
+	}
 }
