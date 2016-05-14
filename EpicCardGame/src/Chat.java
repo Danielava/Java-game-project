15da93533c88dd1f61877f878d48babd25679ecc
TheForgot3n1 <gerdano_jetmotor@hotmail.com>
2016-05-14 12:20:09 +0200

    New Chat class constructor

---
+++ EpicCardGame/src/Chat.java
@@ -0,0 +1,25 @@
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import java.awt.Dimension;
+import java.util.ArrayList;
+
+public class Chat extends GameLoopChat{
+	
+	public Chat(Group root, double w, double h) {
+		screenWidth = w;
+		screenHeight = h;
+
+		canvasPosX = screenWidth * 0.016; //rita från denna position
+		canvasPosY = screenHeight * 0.2;
+		
+		canvas = new Canvas(canvasPosX + 102, canvasPosY + 112); //canvas storlek i parametrarna (till denna position)
+		root.getChildren().add(canvas);
+
+		canvas.setTranslateX(canvasPosX);
+		canvas.setTranslateY(canvasPosY);
+
+		gc = canvas.getGraphicsContext2D();
+	}
+}
