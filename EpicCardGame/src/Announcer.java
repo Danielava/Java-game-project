8831587f8d554495c23da702c5f284b5710ae7b1
TheForgot3n1 <gerdano_jetmotor@hotmail.com>
2016-05-17 20:36:45 +0200

    Add Announcer interface for text senders

---
+++ EpicCardGame/src/Announcer.java
@@ -0,0 +1,9 @@
+/**
+ * Promises to have a method that writes informative text to the chat class.
+ * @author TheForgot3n1
+ *
+ */
+public interface Announcer {
+
+	public void announceEvents();
+}
