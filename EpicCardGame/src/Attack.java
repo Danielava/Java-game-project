b4fabf0fbfd3cc897de4e7611b8997a2415583dc
Daniel <dpar@kth.se>
2016-06-13 18:05:48 +0200

    add attack code for AI

--- EpicCardGame/src/Attack.java
+++ EpicCardGame/src/Attack.java
@@ -1,6 +1,7 @@
 import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * The class has a fundamental method used in a gameloop
@@ -15,16 +16,19 @@ public class Attack {
     private Dice dice;
 
     private Board userBoard, opponentBoard;
-    private ArrayList<Card> userCardsOnBoard, opponentCardsOnBoard;
 
     private Card pickedCard;
+    private Card card1, card2;
+    private VBox c1, c2;
 
-    private boolean check;
+    private boolean check, wait;
+    private Random rnd;
 
     public Attack(GameLoopUser you, OpponentAI ai) {
+        rnd = new Random();
         user =  you;
         this.ai = ai;
-
+        wait = false;
         press = true;
 
         dice = you.getDice();
@@ -33,33 +37,40 @@ public class Attack {
         opponentBoard = ai.getOpponentBoard();
 
         //remember that the cards on board will change when cards are added/removed etc
-        setCardsOnBoard();
         check = true;
     }
 
-    private void setCardsOnBoard() {
+    /**
+     * se till att anropa denna metod för att uppdatera infon.
+     */
+    /*
+    public void setCardsOnBoard() {
         userCardsOnBoard = userBoard.cardsOnBoard();
         opponentCardsOnBoard = opponentBoard.cardsOnBoard();
     }
+    */
 
     //put in loop after diceThrow and if BOARD.CHECK_BOARD_MATCH is true.
     public void pickAttackCardEvent() {
-        if(press) { //good position?
-            for (Card c : userCardsOnBoard) {
+        if (press && !wait) { //good position?
+            wait = true;
+
+            for (Card c : userBoard.cardsOnBoard()) {
                 VBox card = c.getVBoxCard();
                 if (c.getAttackStatus()) {
                     card.setOnMouseClicked(e -> {
                         //du trycker på ditt kort en gång
                         if (c.getAttackStatus() && press) { //&&press //attack status is only true if card is matching dice number and then we enter this if-statemen
-                            System.out.println("HEJ");
                             card.getStyleClass().add("vboxPicked");
                             pickedCard = c;
                             press = false;
+                            wait = false;
                         }
                     });
                 } else {
                     card.getStyleClass().add("vbox"); //doesn't work
                 }
+                wait = false;
             }
         }
     }
@@ -69,13 +80,12 @@ public class Attack {
      */
     public void attackOpponentEvent() {
         if(!press && pickedCard.getAttackStatus()) {
-            for(Card c : opponentCardsOnBoard) {
+            for(Card c : opponentBoard.cardsOnBoard()) {
                 VBox card = c.getVBoxCard();
 
                 card.setOnMouseClicked(e -> {
                     //attack opponent
-                    press = true;
-                    attack(pickedCard, c, dice.getDiceNumber()); //why is this still being called when we sell variables to false?
+                    attack(pickedCard, c, dice.getDiceNumber(), false); //why is this still being called when we sell variables to false?
                     pickedCard.setAttackStatus(false);
                     pickedCard.setHasAttacked(true);
                     //try and call the attack method inside the GameLoopUser loop.
@@ -85,9 +95,20 @@ public class Attack {
     }
 
     /**
+     * Call this method in the AI for attacking.
+     * So far it's built very simple, the AI will just
+     * attack one random card from your board.
+     */
+    public void aiAttack(Card attacker, int diceNr) {
+
+        int rndCard = rnd.nextInt(userBoard.getSize());
+        attack(attacker, userBoard.getCard(rndCard), diceNr, true);
+    }
+
+    /**
      * Helper method attack for attackOpponentEvent()
      */
-    public void attack(Card attacker, Card victim, int diceNr) {
+    public void attack(Card attacker, Card victim, int diceNr, boolean AI) {
         ArrayList<Spell> spells = attacker.getSpells(); //all spells in this list
         for(Spell s : spells) {
             if(s.getDiceNumber() == diceNr && !attacker.getHasAttacked()) {
@@ -95,14 +116,17 @@ public class Attack {
                 victim.updateHpOnCard();
                 //System.out.println(victim.getCurrentHealth());
                 if(victim.getCurrentHealth() <= 0) {
-                    opponentBoard.removeFromBoard(victim);
-                    opponentBoard.drawBoard();
+                    if(!AI) {
+                        opponentBoard.removeFromBoard(victim);
+                        opponentBoard.drawBoard();
+                    } else {
+                        userBoard.removeFromBoard(victim);
+                        userBoard.drawBoard();
+                    }
+                    victim.killCard(); //sets its dead variable to true. not needed I think..
                     victim.remove();
-                    GameLoopUser.killCount += 1;
-                    //System.out.println(attacker.getAttackStatus()); //why
                 }
-                attacker.setAttackStatus(false);
-                attacker.setDefaultCardStyle();
+                attacker.setAttackStatus(false); //när attack status är false ska kortet ej kunna attackera mer under omgången.
                 attacker.setHasAttacked(true);
             }
         }
@@ -112,5 +136,8 @@ public class Attack {
         return press;
     }
 
+    public void resetPress() {
+        press = true;
+    }
 
 }
